a6f565c666de104d7a96e77c3856736952177ac0
jyami-kim <mor2222@naver.com>
2021-12-11 17:33:23 +0900

    feat: base ktor server init

---
+++ src/main/kotlin/com/jyami/Application.kt
@@ -0,0 +1,11 @@
+package com.jyami
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.jyami.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/com/jyami/plugins/Routing.kt
@@ -0,0 +1,14 @@
+package com.jyami.plugins
+
+import io.ktor.routing.*
+import io.ktor.application.*
+import io.ktor.response.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
