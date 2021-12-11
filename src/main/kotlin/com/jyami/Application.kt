b58989ff3a261a33259583277dee9296f87160b8
jyami-kim <mor2222@naver.com>
2021-12-11 22:25:06 +0900

    feat: impletement order routing

--- src/main/kotlin/com/jyami/Application.kt
+++ src/main/kotlin/com/jyami/Application.kt
@@ -1,6 +1,7 @@
 package com.jyami
 
 import com.jyami.plugins.*
+import com.jyami.route.registerOrderRoute
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.serialization.*
@@ -11,6 +12,7 @@ fun main() {
     embeddedServer(Netty, port = 9090, host = "0.0.0.0") {
         configureRouting()
         registerCustomerRoutes()
+        registerOrderRoute()
         install(ContentNegotiation){
             json()
         }
---
+++ src/main/kotlin/com/jyami/route/OrderRouting.kt
@@ -0,0 +1,47 @@
+package com.jyami.route
+
+import com.jyami.models.orderStorage
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+
+fun Application.registerOrderRoute(){
+    routing {
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+    }
+}
+
+fun Route.listOrdersRoute() {
+    get("order") {
+        if (orderStorage.isNotEmpty()) {
+            call.respond(orderStorage)
+        }
+    }
+}
+
+fun Route.getOrderRoute() {
+    get("/order/{id}") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        call.respond(order)
+    }
+}
+
+fun Route.totalizeOrderRoute() {
+    get("/order/{id}/total") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        val total = order.contents.map { it.price * it.amount }.sum()
+        call.respond(total)
+    }
+}
